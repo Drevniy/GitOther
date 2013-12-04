c7b092ab8ce24f4c79705e27ac1c0228c6dbf908
Andrew Stasenko <andryuhastasenko@gmail.com>
2013-12-04 19:32:26 +0200

    Hibernate had some diprecated method

--- PersonAndManyDB/src/db/HibernateUtil.java
+++ PersonAndManyDB/src/db/HibernateUtil.java
@@ -2,14 +2,20 @@ package db;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
 public class HibernateUtil {
     private static SessionFactory sessionFactory = null;
+    private static ServiceRegistry serviceRegistry;
     
     static {
         try {
                 //creates the session factory from hibernate.cfg.xml
-                sessionFactory = new Configuration().configure().buildSessionFactory();
+        	Configuration configuration = new Configuration();
+            configuration.configure();
+            serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();  
+                sessionFactory = new Configuration().buildSessionFactory(serviceRegistry);
         } catch (Exception e) {
               e.printStackTrace();
         }
